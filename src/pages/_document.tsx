a74441e9ced3fbf8c2c4986691fe1c34e7bf7f1c
sicoding1120 <daffahafizhfirdaus07@gmail.com>
2024-06-11 10:38:58 +0700

    update

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,12 @@
+/* eslint-disable @next/next/no-title-in-document-head */
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
     <Html lang="en" className='scroll-smooth'>
-      <Head />
+      <Head>
+        <link rel="icon" type="image/x-icon" href="/assets/logo/favicon.png" />
+      </Head>
       <body>
         <Main />
         <NextScript />
