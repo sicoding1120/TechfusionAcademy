b21186030d7c1ae18959d3c3956201f259163c6d
sicoding1120 <daffahafizhfirdaus07@gmail.com>
2024-07-11 08:51:23 +0700

    update meta tag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,6 +6,9 @@ export default function Document() {
     <Html lang="en" className='scroll-smooth'>
       <Head>
         <link rel="icon" type="image/x-icon" href="favicon.png" />
+        <meta name="description" content="web untuk belajar programming berbahasa indoneia terbaru" />
+        <meta name="keywords" content="tech, fusion ,tech fusion, techfusion,techfusion academy, academy, ui/ux, html5, css, belajar coding, javascript learn, next learn" />
+        <meta name='author' content='daffa hafizh firdaus dan ammar rizki adia permono' />
         <title>Techfusion Academy</title>
       </Head>
       <body>
